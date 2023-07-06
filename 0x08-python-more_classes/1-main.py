828d92c2a7cf63f6fd54c16c1834b3c74520b9d8
noiseworking <you@example.com>
2023-07-05 17:24:05 -0700

    updating

--- 0x08-python-more_classes/1-main.py
+++ 0x08-python-more_classes/1-main.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-Rectangle = __import__('1-rectangle').Rectangle
+Rectangle = __import__('0-rectangle').Rectangle
 
-my_rectangle = Rectangle(2, 4)
-print(my_rectangle.__dict__)
-
-my_rectangle.width = 10
-my_rectangle.height = 3
+my_rectangle = Rectangle()
+print(type(my_rectangle))
 print(my_rectangle.__dict__)

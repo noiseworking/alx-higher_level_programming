b3b0890385724c737d233495203c32ea659de031
noiseworking <you@example.com>
2023-07-11 18:16:29 -0700

    updating

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
-"""Module: 0-read_file
-This is a function that reads a text file (UTF8) and prints it to stdout
-"""
+"""Defines a text file-reading function."""
+
+
 def read_file(filename=""):
-    """Reads a text file (UTF8) and prints it to stdout
-
-    Args:
-        - filename: this is the file to be read
-    """   
-
-    with open(filename) as f:
-        read_data = f.read()
-        print(read_data, end="")
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")

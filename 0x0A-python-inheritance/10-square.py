901c8919675ba53ec576bc670334f593b7b6393f
noiseworking <you@example.com>
2023-07-12 18:58:06 -0700

    updating

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
-Square = __import__('10-square').Square
+"""
+Contains the class BaseGeometry and subclass Rectangle
+"""
 
-s = Square(13)
+Rectangle = __import__('9-rectangle').Rectangle
 
-print(s)
-print(s.area())
+
+class Square(Rectangle):
+    """A representation of a square"""
+    def __init__(self, size):
+        """instantiation of the square"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """"returns the area of the square"""
+        return self.__size ** 2
